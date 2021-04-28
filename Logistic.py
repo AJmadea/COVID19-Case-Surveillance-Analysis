45056e0cdf8053fc99b8658b9a2f50c6c6fedf17
Andrew <andrewmadea@gmail.com>
2021-04-28 00:32:23 -0400

    Created create_map_ranking().  It iterates through rfe.ranking_ creates a dictionary of ranks as keys and a list of columns as the value.

--- Logistic.py
+++ Logistic.py
@@ -2,33 +2,57 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFECV
 import pandas as pd
 import ModelMethods as mm
 
 
 def use_rfe(_data, x_columns, target):
-    _data = _data[x_columns]
-    uniques = mm.get_different_dummies_columns(_data[[target]])
-    dummies = pd.get_dummies(_data, columns=x_columns)
+    x, y = mm.create_x_y(_data, x_columns, target)
 
-    x_dummies = dummies.columns.tolist().copy()
-    for col in dummies.columns:
-        if col in uniques:
-            x_dummies.remove(col)
+    print(x.shape)
+    print(x.columns)
 
-    x = dummies[x_dummies]
-    y = dummies[uniques[0]]
-
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, train_size=.7)
-    lm = LogisticRegression(n_jobs=-1, verbose=True, max_iter=1000000)
-    print("Creating RFECV")
-    rfe = RFE(estimator=lm, verbose=True, )
+    lm = LogisticRegression(n_jobs=-1, verbose=True, max_iter=10_000)
+    print("Creating RFE")
+    rfe = RFE(estimator=lm, verbose=True)
     print("Fitting training data")
-    rfe.fit(xTrain, yTrain)
+    rfe.fit(x, y)
     print(rfe.ranking_)
+    m = create_map_ranking(rfe.ranking_, x.columns)
+
+    for k in m.keys():
+        print(k, '\t', m.get(k))
 
 
+def use_rfecv(_data, x_columns, target):
+    x, y = mm.create_x_y(_data, x_columns, target)
 
+    print(x.shape)
+    print(x.columns)
+
+    lm = LogisticRegression(n_jobs=-1, verbose=True, max_iter=10_000)
+    print("Creating RFE")
+    rfe = RFECV(estimator=lm, verbose=True, cv=4)
+    print("Fitting training data")
+    rfe.fit(x, y)
+    print(rfe.ranking_)
+    m = create_map_ranking(rfe.ranking_, x.columns)
+
+    for k in m.keys():
+        print(k, '\t', m.get(k))
+
+
+def create_map_ranking(rankings, columns):
+    map = {}
+    for i, c in enumerate(rankings):
+        if c in map.keys():
+            t = map.get(c)
+            t.append(columns[i])
+            map[c] = t
+        else:
+            map[c] = [columns[i]]
+    return map
 
 
 def iterate_through_dummies(_data, x_columns, target):
